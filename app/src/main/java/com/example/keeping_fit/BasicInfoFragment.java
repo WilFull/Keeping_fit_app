0fc7ade13ad349625a3e51f5e35b6572880d740d
yuki <kryukovaolga123gmail.com>
2022-05-18 22:40:01 +0300

    Сделана кнопка "Обновить данные" в разделе "Здоровье"

---
+++ app/src/main/java/com/example/keeping_fit/BasicInfoFragment.java
@@ -0,0 +1,52 @@
+package com.example.keeping_fit;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class BasicInfoFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    // метод для отображения  health_layout во фрагменте HealthFragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.basic_info, container, false);
+
+        // кнопка Продолжить
+        Button btnСntToMainMenu = (Button) view.findViewById(R.id.btnСntToMainMenu); // объявляем кнопку
+        btnСntToMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HealthFragment healthFragment = new HealthFragment();
+                ((MainMenuActivity)getActivity()).setNewFragment(healthFragment);
+            }
+        });
+
+        // кнопка Пропустить
+        Button btnSkipToMainMenu = (Button) view.findViewById(R.id.btnSkipToMainMenu); // объявляем кнопку
+        btnSkipToMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HealthFragment healthFragment = new HealthFragment();
+                ((MainMenuActivity)getActivity()).setNewFragment(healthFragment);
+            }
+        });
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/keeping_fit/HealthFragment.java
+++ app/src/main/java/com/example/keeping_fit/HealthFragment.java
@@ -14,11 +14,28 @@ import android.widget.Button;
 
 public class HealthFragment extends Fragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     // метод для отображения  health_layout во фрагменте HealthFragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.health_layout, container, false);
+        View view = inflater.inflate(R.layout.health_layout, container, false);
+
+        // кнопка Обновить данные
+        Button btnUpdateData = (Button) view.findViewById(R.id.btnUpdateData); // объявляем кнопку
+        btnUpdateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BasicInfoFragment basicInfoFragment = new BasicInfoFragment();
+                ((MainMenuActivity)getActivity()).setNewFragment(basicInfoFragment);
+            }
+        });
+
+        return view;
     }
 
 
